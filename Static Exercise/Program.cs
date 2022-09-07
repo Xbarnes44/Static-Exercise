f70a7a878d64c26ce5d24c5004cffc9b6a037f09
Xavier Barnes <xdbcoder44@gmail.com>
2022-09-07 10:34:36 -0400

    Add project files.

---
+++ Static Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using Static_Exercise;
+using System;
+
+namespace StaticExercise
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{TempConverter.FahrenheitToCelsius(50.0)}");
+
+            Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(10.0)}");
+        }
+
+       
+    }
+}
---
+++ Static Exercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Exercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fardegree)
+        {
+            return (Fardegree -32) *.5556;
+        }
+
+        public static double CelsiusToFahrenheit(double Fardegree)
+        {
+            return Fardegree *1.8 + 32;
+        }
+
+    }
+   
+}
